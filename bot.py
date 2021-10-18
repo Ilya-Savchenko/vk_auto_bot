c9ad307b80359c79d403d738d8ae851439b5fb34
ilya-Savchenko <ilya07.iv@gmail.com>
2021-10-18 20:36:19 +0300

    implemented state0

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 import states
 import settings
 from database.create_db import connect_and_fill_db
-from database.models import UserState
+from database.models import UserState, Section
 from message_handler import MessageHandler
 
 
@@ -17,12 +17,16 @@ class VkBot:
         self.api = vk_api.VkApi(token=self.token)
         self.poller = VkBotLongPoll(self.api, self.group_id)
         self.vk_sess = self.api.get_api()
-        self.msg_handler = MessageHandler  # обработка сообщений
+        self.categories = None
+        self.msg_handler = MessageHandler()  # обработка сообщений
         self.message_sender = None  # отправка сообщений
 
     def run(self):
         """Start bot"""
         connect_and_fill_db()
+        print(states.generate_tokens_for_state1())
+        self.categories = [category.name for category in Section.select()]
+        print(self.categories)
         print('run')
         for event in self.poller.listen():
             self.event_handler(event)
@@ -30,38 +34,43 @@ class VkBot:
 
     def event_handler(self, event):
         if event.type == VkBotEventType.MESSAGE_NEW:
+            print('event')
             message = event.object.message
             user_id = str(message.get('from_id'))
             message_text = message.get('text')
 
             query = UserState.select().where(UserState.user_id == user_id)
             if not query.exists():
-                self._start(user_id=user_id)
+                self._start(user_id=user_id, msg=message_text)
             else:
-                self._continue(user_id=user_id)
-
-            msg = self.handle_message(message_text)
-            self.send_text_message(user_id=user_id, msg=msg)
+                self._continue(user_id=user_id, msg=message_text)
 
 
-    def handle_message(self, message_text):
-        handler = self.msg_handler(message_text)
-        msg = handler.handle_message()
-        return msg
-
-    def _start(self, user_id):
+    def _start(self, user_id, msg):
         state = states.STATES.get('first_state')
-        UserState.create(user_id=user_id, state=state)
+        handler_name = states.STATES.get('states').get(state).get('handler')
+        handler = getattr(self.msg_handler, handler_name)
+        text = handler(msg=msg, user_id=user_id)
+        self.send_text_message(user_id, text)
+        print('start')
+        # for i in range(len(self.categories)):
+        #     text += f'\n{i + 1}. {self.categories[i]}'
 
-    def _continue(self, user_id):
+
+    def _continue(self, user_id, msg):
+        all_states = states.STATES.get('states')
         state = UserState.get(UserState.user_id == user_id).state
-        next_state = states.STATES.get('states').get(state).get('next_state')
-        previous_state = states.STATES.get('states').get(state).get('previous_state')
+        next_state = all_states.get(state).get('next_state')
+        print('continue')
+        new_state = UserState.get(UserState.user_id == user_id)
+        new_state.state = next_state
+        # previous_state = states.STATES.get('states').get(state).get('previous_state')
+        all_states = states.STATES.get('states')
+        text = all_states.get(next_state).get('text')
+        self.send_text_message(user_id, text)
 
     def send_text_message(self, user_id, msg):
         self.vk_sess.messages.send(user_id=user_id, message=msg, random_id=vk_api.utils.get_random_id())
-
-
 
 
 if __name__ == '__main__':
--- message_handler.py
+++ message_handler.py
@@ -1,21 +1,22 @@
-from database.models import Good, Section
+from database.models import Good, Section, UserState
+import states
 
 class MessageHandler:
 
-    def __init__(self, message):
-        self.msg = message
+    def state0_msg_handler(self, msg, user_id):
+        state = states.STATES.get('first_state')
+        all_states = states.STATES.get('states')
+        next_state = all_states.get(state).get('next_state')
+        if msg.lower() == all_states.get(state).get('tokens'):
+            text = all_states.get(next_state).get('text')
+            UserState.create(user_id=user_id, state=next_state)
+        else:
+            text = all_states.get(state).get('error_text')
+        return text
 
-    def handle_message(self):
-        if self.msg.lower() == 'начать':
-            sections_set = Section.select()
-            sections = []
-            for el in sections_set:
-                sections.append(el.name)
-            return sections
+    def state1_msg_handler(self):
+        pass
 
-    def msg_handler(self, msg, state):
-        if str(msg).isdigit() and state=='state1':
-            if int(msg) < len(state):
-                return True
-
+    def state2_msg_handler(self):
+        pass
 
