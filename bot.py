aede662700189c1fe2720c25a57769d9001d7b3f
ilya-Savchenko <ilya07.iv@gmail.com>
2021-10-22 19:42:41 +0300

    fix style

--- bot.py
+++ bot.py
@@ -1,10 +1,9 @@
-import requests
 import vk_api
 import vk_api.utils
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 
-import states
 import settings
+import states
 from database.create_db import connect_and_fill_db
 from database.models import UserState, Section
 from message_handler import MessageHandler
@@ -29,7 +28,6 @@ class VkBot:
         for event in self.poller.listen():
             self.event_handler(event)
 
-
     def event_handler(self, event):
         if event.type == VkBotEventType.MESSAGE_NEW:
             print('event')
@@ -43,15 +41,12 @@ class VkBot:
             else:
                 self._continue(msg=message_text, user_id=user_id)
 
-
     def _start(self, user_id, msg):
         state = states.STATES.get('first_state')
         handler_name = states.STATES.get('states').get(state).get('handler')
         handler = getattr(self.msg_handler, handler_name)
         text = handler(msg=msg, user_id=user_id)
         self.send_text_message(user_id, text)
-
-
 
     def _continue(self, msg, user_id):
         state = UserState.get(UserState.user_id == user_id).state
@@ -61,17 +56,6 @@ class VkBot:
         if image:
             self.send_image_message(user_id, image)
         self.send_text_message(user_id, text)
-        # self.msg_handler.state1_msg_handler(msg=msg, user_id=user_id)
-        # all_states = states.STATES.get('states')
-        # next_state = all_states.get(state).get('next_state')
-        # print('continue')
-        # new_state = UserState.get(UserState.user_id == user_id)
-        # new_state.state = next_state
-        # # previous_state = states.STATES.get('states').get(state).get('previous_state')
-        # all_states = states.STATES.get('states')
-        # text = all_states.get(next_state).get('text')
-        # self.send_text_message(user_id, text)
-
 
     def send_text_message(self, user_id, msg):
         self.vk_sess.messages.send(user_id=user_id, message=msg, random_id=vk_api.utils.get_random_id())
@@ -81,20 +65,8 @@ class VkBot:
         img = upload.photo_messages(image)
         owner_id = img[0]['owner_id']
         media_id = img[0]['id']
-        # access_key = img[0]['access_key']
-        # attachment = f'photo{owner_id}_{media_id}_{access_key}'
         attachment = f'photo{owner_id}_{media_id}'
         self.vk_sess.messages.send(user_id=user_id, attachment=attachment, random_id=vk_api.utils.get_random_id())
-
-        # upload_url = self.vk_sess.photos.getMessagesUploadServer().get('upload_url')
-        # print(upload_url)
-        # upload_data = requests.post(upload_url, files={'photo': ('image.jpg', image, 'image/jpeg')}).json()
-        # print(upload_data)
-        # image_data = self.vk_sess.photos.saveMessagesPhoto(**upload_data)
-        #
-        # owner_id = image_data[0]['owner_id']
-        # media_id = image_data[0]['id']
-        # self.vk_sess.messages.send(user_id=user_id, attachment=attachment, random_id=vk_api.utils.get_random_id())
 
 
 if __name__ == '__main__':
